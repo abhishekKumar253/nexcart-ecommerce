225d1880267935ad37d63a2bf5d60866ed83cb9a
abhishekkumar4553 <abhishekgupta45591@gmail.com>
2026-05-15 01:27:04 +0530

    refactor: simplify JSX structure in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,19 @@
 import { Show, SignInButton, SignUpButton, UserButton } from "@clerk/react";
+
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <header>
-        <Show when="signed-out">
-          <SignInButton mode="modal" />
-          <SignUpButton mode="modal"/>
-        </Show>
-        <Show when="signed-in">
-          <UserButton />
-        </Show>
-      </header>
-    </>
+    <header>
+      <Show when="signed-out">
+        <SignInButton mode="modal" />
+        <SignUpButton mode="modal" />
+      </Show>
+
+      <Show when="signed-in">
+        <UserButton />
+      </Show>
+    </header>
   );
 }
 
